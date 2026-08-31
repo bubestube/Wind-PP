2c1860e417b367080513da28d1f373835391d626
bubestube <mbwana@gmail.com>
2026-08-31 10:44:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -159,7 +159,7 @@ if os.path.exists(CSV_FILE):
                 ayref="pixel",
                 showarrow=True,
                 arrowhead=2,
-                arrowsize=1.1,   # Slimmer arrowhead
+                arrowsize=1.4,   # Slimmer arrowhead
                 arrowwidth=2.0,  # Slimmer stem
                 arrowcolor=arrow_color,
                 opacity=0.95
