119c2ba68a2f03fc783f7b96b680d17932cb6341
bubestube <mbwana@gmail.com>
2026-08-31 14:44:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -400,8 +400,8 @@ if df is not None and not df.empty:
             ayref="pixel",
             showarrow=True,
             arrowhead=2,
-            arrowsize=0.8,
-            arrowwidth=1.2,
+            arrowsize=1,
+            arrowwidth=1,
             arrowcolor="#0f172a",
             opacity=0.95
         )
